6ccac8834aa59b692366dc26063716ebb666a273
Heath Shults <heatheshults@gmail.com>
2017-09-18 17:35:35 -0500

    added page loader to sign in, added header tag injector

--- js/bss.js
+++ js/bss.js
@@ -1,5 +1,12 @@
 // include in pages
-$(document).ready(function() {
-  $('#head-nav-01').load('./components/header.html');
-  $('#footer-nav-01').load('./components/footer.html');
+$(document).ready(function () {
+  if ($('#headtags-01').length) {
+    $('#headtags-01').load('./components/headtags.html');
+  }
+  if ($('#head-nav-01').length) {
+    $('#head-nav-01').load('./components/header.html');
+  }
+  if ($('#footer-nav-01').length) {
+    $('#footer-nav-01').load('./components/footer.html');
+  }
 });
