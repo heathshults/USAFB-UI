753b588767df23baf9082bafcd086690eefe301b
Heath Shults <heatheshults@gmail.com>
2017-09-27 10:27:57 -0500

    profile page refactor, user table search and filter

--- js/bss.js
+++ js/bss.js
@@ -1,5 +1,17 @@
-// page function and effects
-$(document).ready(function () {
+$('document').click(function () {
   $('body').css('display', 'none')
   $('body').fadeIn(500)
+})
+
+$('.click-to-fade').click(function fadeAway () {
+  $('body').fadeOut(500)
+})
+
+$('.status-enabled').click(function () {
+  $(this).removeClass('status-enabled')
+  $(this).addClass('status-disabled')
+})
+$('.status-disabled').click(function () {
+  $(this).removeClass('status-disabled')
+  $(this).addClass('status-enabled')
 })
