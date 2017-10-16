c78d15ed371d335d8249cbdd04f39a5128b4c309
heathshults <heatheshults@gmail.com>
2017-10-16 17:55:47 -0500

    added event function css and new dnd upload modal css

--- js/bss.js
+++ js/bss.js
@@ -1,22 +1,23 @@
-$('document').click(function () {
-  $('body').css('display', 'none')
-  $('body').fadeIn(500)
-})
+$('document').click(function() {
+  $('body').css('display', 'none');
+  $('body').fadeIn(500);
+});
 
-$('.click-to-fade').click(function fadeAway () {
-  $('body').fadeOut(500)
-})
+$('.click-to-fade').click(function fadeAway() {
+  $('body').fadeOut(500);
+});
 
-$('.status-enabled').click(function () {
-  $(this).removeClass('status-enabled')
-  $(this).addClass('status-disabled')
-})
-$('.status-disabled').click(function () {
-  $(this).removeClass('status-disabled')
-  $(this).addClass('status-enabled')
-})
+$('.status-enabled').click(function() {
+  $(this).removeClass('status-enabled');
+  $(this).addClass('status-disabled');
+});
+$('.status-disabled').click(function() {
+  $(this).removeClass('status-disabled');
+  $(this).addClass('status-enabled');
+});
 
 // enable popovers
-$(function () {
-  $('[data-toggle="popover"]').popover()
-})
+$(function() {
+  $('[data-toggle="popover"]').popover();
+});
+
