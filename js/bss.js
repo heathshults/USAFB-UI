9805149643256a7f9d19e37e5c1651c6af46a305
heathshults <heatheshults@gmail.com>
2017-11-02 18:07:15 -0500

    new status edit modal

--- js/bss.js
+++ js/bss.js
@@ -1,26 +1,27 @@
-$('document').click(function() {
-  $('body').css('display', 'none');
-  $('body').fadeIn(500);
-});
+(function ($) {
 
-$('.click-to-fade').click(function fadeAway() {
-  $('body').fadeOut(500);
-});
+$('document').click(() => {
+  $('body').css('display', 'none')
+  $('body').fadeIn(500)
+})
 
-$('.status-enabled').click(function() {
-  $(this).removeClass('status-enabled');
-  $(this).addClass('status-disabled');
-});
-$('.status-disabled').click(function() {
-  $(this).removeClass('status-disabled');
-  $(this).addClass('status-enabled');
-});
+$('.click-to-fade').click(() => {
+  $('body').fadeOut(500)
+})
+
+$('.status-enabled').click(function () {
+  $(this).removeClass('status-enabled')
+  $(this).addClass('status-disabled')
+})
+$('.status-disabled').click(function () {
+  $(this).removeClass('status-disabled')
+  $(this).addClass('status-enabled')
+})
 
 // enable popovers
-$(function() {
-  $('[data-toggle="popover"]').popover();
-});
-
-//////////////////
+$(() => {
+  $('[data-toggle="popover"]').popover()
+})
 
 
+}(jQuery))
