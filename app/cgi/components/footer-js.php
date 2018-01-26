b4e678f76c03f6198ecf19dcb5e702b136599739
heathshults <heatheshults@gmail.com>
2018-01-26 09:42:38 -0600

    sign in page layout change and other tweaks

---
+++ app/cgi/components/footer-js.php
@@ -0,0 +1,15 @@
+<script src="./js/vendor/jquery/jquery-3.2.1.min.js"></script>
+<script>
+  window.jQuery || document.write('<script src="./js/vendor/jquery/jquery-1.12.0.min.js"><\/script>')
+
+</script>
+<script src="./js/vendor/popper.js"></script>
+<script src="./js/usafb-bootstrap.js"></script>
+<script src="./js/vendor/modernizr-2.8.3.min.js"></script>
+<!-- Page Loader -->
+<script src="./js/vendor/pace/v1.0.0/pace.min.js"></script>
+<!-- Bootstrap-table -->
+<script src="./js/vendor/bootstrap-table/1.11.1/bootstrap-table.js"></script>
+<script src="./js/vendor/bootstrap-table/1.11.1/locale/bootstrap-table-en-US.min.js"></script>
+<script src="./js/vendor/dropzone/dropzone.js"></script>
+<script src="./js/bss.js"></script>
